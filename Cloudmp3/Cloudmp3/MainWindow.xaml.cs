a728e0b1770005c876c4f245b83477ed93024b91
ArsenicStealth <13sdpuckett@live.com>
2014-08-21 19:46:45 -0600

    small c

--- Cloudmp3/Cloudmp3/MainWindow.xaml.cs
+++ Cloudmp3/Cloudmp3/MainWindow.xaml.cs
@@ -55,7 +55,7 @@ namespace Cloudmp3
 
 		public MainWindow()
 		{
-			//MetaLogic();
+			MetaLogic();
 			try
 			{
 				InitializeComponent();
@@ -281,44 +281,44 @@ namespace Cloudmp3
 			notifLabel.Content = "Download Complete";
 			notifarea.Visibility = Visibility.Hidden;
 		}
-		//private void MetaLogic()
-		//{
-		//    byte[] b = new byte[128];
-		//    string sTitle;
-		//    string sSinger;
-		//    string sAlbum;
-		//    string sYear;
-		//    string sComm;
+		private void MetaLogic()
+		{
+			byte[] b = new byte[128];
+			string sTitle;
+			string sSinger;
+			string sAlbum;
+			string sYear;
+			string sComm;
 
-		//    FileStream fs = new FileStream("C:\\Users\\Scott Puckett\\Desktop\\Folders", FileMode.Open);
-		//    fs.Seek(-128, SeekOrigin.End);
-		//    fs.Read(b, 0, 128);
-		//    bool isSet = false;
-		//    String sFlag = System.Text.Encoding.Default.GetString(b, 0, 3);
-		//    if (sFlag.CompareTo("TAG") == 0)
-		//    {
-		//        System.Console.WriteLine("Tag   is   setted! ");
-		//        isSet = true;
-		//    }
+            FileStream fs = new FileStream("C:\\Users\\Scott Puckett\\Desktop\\Folders\\Scott_Pers.MP3", FileMode.Open);
+			fs.Seek(-128, SeekOrigin.End);
+			fs.Read(b, 0, 128);
+			bool isSet = false;
+			String sFlag = System.Text.Encoding.Default.GetString(b, 0, 3);
+			if (sFlag.CompareTo("TAG") == 0)
+			{
+				System.Console.WriteLine("Tag   is   setted!");
+				isSet = true;
+			}
 
-		//    if (isSet)
-		//    {
-		//        //get   title   of   song;
-		//        sTitle = System.Text.Encoding.Default.GetString(b, 3, 30);
-		//        System.Console.WriteLine("Title: " + sTitle);
-		//        //get   singer;
-		//        sSinger = System.Text.Encoding.Default.GetString(b, 33, 30);
-		//        System.Console.WriteLine("Singer: " + sSinger);
-		//        //get   album;
-		//        sAlbum = System.Text.Encoding.Default.GetString(b, 63, 30);
-		//        System.Console.WriteLine("Album: " + sAlbum);
-		//        //get   Year   of   publish;
-		//        sYear = System.Text.Encoding.Default.GetString(b, 93, 4);
-		//        System.Console.WriteLine("Year: " + sYear);
-		//        //get   Comment;
-		//        sComm = System.Text.Encoding.Default.GetString(b, 97, 30);
-		//        System.Console.WriteLine("Comment: " + sComm);
-		//    }
-		//}
+			if (isSet)
+			{
+				//get   title   of   song;
+				sTitle = System.Text.Encoding.Default.GetString(b, 3, 30);
+				System.Console.WriteLine("Title: " + sTitle);
+				//get   singer;
+				sSinger = System.Text.Encoding.Default.GetString(b, 33, 30);
+				System.Console.WriteLine("Singer: " + sSinger);
+				//get   album;
+				sAlbum = System.Text.Encoding.Default.GetString(b, 63, 30);
+				System.Console.WriteLine("Album: " + sAlbum);
+				//get   Year   of   publish;
+				sYear = System.Text.Encoding.Default.GetString(b, 93, 4);
+				System.Console.WriteLine("Year: " + sYear);
+				//get   Comment if any exist;
+				sComm = System.Text.Encoding.Default.GetString(b, 97, 30);
+				System.Console.WriteLine("Comment: " + sComm);
+			}
+		}
 	}
 }
